9a81053d4d9bf574a5d8e40e16a7b11543420a1d
WolframKristof <wolfram.kristof@gmail.com>
2021-07-29 14:14:02 +0200

    Login barebone HTML added

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/base-style.css">
+    <title>Farkas Vadkovászos Pékség Admin</title>
+</head>
+<body id="login-screen">
+    <div id="login-panel">
+        <img src="../images/logo.jpg" alt="" srcset="">
+
+        <form action="">
+            <label for="login-username">FELHASZNÁLÓNÉV:</label>
+            <input type="text" id="login-username">
+
+            <label for="login-password">JELSZÓ:</label>
+            <input type="text" id="login-password">
+
+            <button type="submit">Bejelentkezés</button>
+        </form>
+
+    </div>
+</body>
+</html>
