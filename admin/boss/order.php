d80127732f76d9a9eb1111ebc97a8082b4b55aed
WolframKristof <wolfram.kristof@gmail.com>
2021-08-08 16:06:17 +0200

    Loads of stuff

---
+++ admin/boss/order.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/../css/admin-base-style.min.css">
+    <link rel="stylesheet" href="/../css/admin-base-print-style.min.css" media="print">
+    <title>Rendelés</title>
+</head>
+<body class="storage">
+    <header>
+        <p>BOSS</p>
+        <button id="logout-button">Kilépés</button>
+    </header>
+    <nav>
+        <a href="stores.php" class="navigation-option">ÜZLETEK</a>
+        <a href="productions.php" class="navigation-option">GYÁRTÁS</a>
+        <a href="storage.php" class="navigation-option">KÉSZLET</a>
+        <a href="products.php" class="navigation-option">TERMÉKEK</a>
+    </nav>
+    <main>
+        <div id="main-top">
+            <p id="page-title">Rendelés</p>
+            <div id="page-buttons">
+                <button class="page-button primary" onclick="window.print()">Nyomtatás</button>
+                <a class="page-button secondary" href="storage.php">Vissza</a>
+            </div>
+        </div>
+        <div id="main-content">
+            <table class="order-table">
+                <thead>
+                    <tr>
+                        <th><p>TERMÉK</p></th>
+                        <th><p>MENNYISÉG</p></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <p>very longen névvvvvvv az jóóó vagy nem</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ÍKeszike</p>
+                        </td>
+                        <td>
+                            <p>5</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+    <script src="/../js/main-script.js"></script>
+</body>
+</html>
