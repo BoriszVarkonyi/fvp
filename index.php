62fbecdb826650544b44e85f0eb544b8e888563a
WolframKristof <wolfram.kristof@gmail.com>
2021-09-06 21:27:15 +0200

    Website in the works

--- index.php
+++ index.php
@@ -10,14 +10,30 @@
 <body>
     <header>
         <nav id="desktop-navigation">
-            <a href="#about" class="desktop-specific">Rólunk</a>
-            <a href="#products" class="desktop-specific">Termékeink</a>
-            <img id="website-logo" src="/../assets/images/logo.svg" alt="Farkas Vadkovászos Pékség Logo">
-            <a href="#stores" class="desktop-specific">Üzleteink</a>
-            <a href="#contact" class="desktop-specific">Elérhetőség</a>
+            <div>
+                <a href="#about" class="desktop-specific">Rólunk</a>
+            </div>
+            <div>
+                <a href="#products" class="desktop-specific">Termékeink</a>
+            </div>
+            <div>
+                <img id="website-logo" src="/../assets/images/logo.svg" alt="Farkas Vadkovászos Pékség Logo">
+            </div>
+            <div>
+                <a href="#stores" class="desktop-specific">Üzleteink</a>
+                <button id="open-nav-button" class="mobile-specific" onclick="openMobileNavigation()">
+                    <img src="/../assets/icons/menu-white.svg" alt="">
+                </button>
+                <button id="close-nav-button" class="mobile-specific hidden" onclick="closeMobileNavigation()">
+                    <img src="/../assets/icons/menu-white.svg" alt="">
+                </button>
+            </div>
+            <div>
+                <a href="#contact" class="desktop-specific">Elérhetőség</a>
+            </div>
         </nav>
     </header>
-    <nav id="mobile-navigation" class="mobile-specific">
+    <nav id="mobile-navigation" class="collapsed">
         <a href="#about">Rólunk</a>
         <a href="#products">Termékeink</a>
         <a href="#stores">Üzleteink</a>
@@ -25,43 +41,54 @@
     </nav>
     <main>
         <div id="show-off">
-            <a href="#main-content-wrapper" id="go-down"><img src="/../assets/icons/expand-more-white.svg" alt=""></a>
+
             <div id="show-off-content">
-                <div>
-                    <p>FARKAS</p>
-                    <p>VADKOVÁSZOS</p>
-                    <p>PÉKSÉG</p>
-                </div>
-                <div>
+                <p class="show-off-text">FARKAS</p>
+                <p class="show-off-text">VADKOVÁSZOS</p>
+                <p class="show-off-text">PÉKSÉG</p>
+
+                <div class="show-off-data">
                     <p><span>12</span> Üzlet</p>
                     <p><span>157</span> Egyedi Termék</p>
                 </div>
+
+                <!--
+                    <div class="show-off-highlight">
+                        <img src="/../assets/icons/priority-high-red.svg" alt="" class="highlight-icon">
+                        <p>Újdonságok</p>
+                        <img src="/../assets/icons/chevron-right-black.svg" alt="" class="highlight-arrow">
+                    </div>
+                 -->
+
             </div>
+
+            <a href="#main-content-wrapper" id="go-down"><img src="/../assets/icons/expand-more-white.svg" alt=""></a>
         </div>
         <div id="main-content-wrapper">
 
-            <div id="main-content">
 
-                <div id="about">
-                    <section>
-                        <h2>Rólunk</h2>
-                    </section>
-
+            <section>
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <h1>Rólunk</h1>
+                    </div>
+                    <div class="content text">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, rerum impedit nemo quas enim officiis sunt doloribus, qui quod quaerat dolorum rem possimus ea, laboriosam nostrum adipisci iste in consequatur.</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit ab odio at beatae vitae libero suscipit reprehenderit sed necessitatibus omnis eos rerum, voluptas maiores laborum amet optio nemo molestias error quas facilis quasi qui. Vel rerum aperiam voluptate ipsum ea non unde repudiandae reiciendis eligendi provident nulla doloremque, perferendis delectus tempora odit nostrum natus tenetur!</p>
+                    </div>
                 </div>
+            </section>
 
-                <div id="products">
+            <section class="light">
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <h1>Termékeink</h1>
+                    </div>
+                    <div class="content grid">
 
-                    <h1>Termékeink</h1>
-                    <section>
-                        <h2>Kategória 1</h2>
-                        <div class="section-content">
-
-                        <div class="section-item">
+                        <div class="section-item highlight">
                             <h3>Kenyér</h3>
-                            <div class="section-item-picture-wrapper">
-                                <img src="/../assets/images/bread-1.jpg" alt="">
-                                <img src="/../assets/images/bread-2.jpg" alt="">
-                            </div>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
                             <div class="section-item-details">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
                             </div>
@@ -69,10 +96,7 @@
 
                         <div class="section-item">
                             <h3>Kenyér</h3>
-                            <div class="section-item-picture-wrapper">
-                                <img src="/../assets/images/bread-1.jpg" alt="">
-                                <img src="/../assets/images/bread-2.jpg" alt="">
-                            </div>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
                             <div class="section-item-details">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
                             </div>
@@ -80,10 +104,7 @@
 
                         <div class="section-item">
                             <h3>Kenyér</h3>
-                            <div class="section-item-picture-wrapper">
-                                <img src="/../assets/images/bread-1.jpg" alt="">
-                                <img src="/../assets/images/bread-2.jpg" alt="">
-                            </div>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
                             <div class="section-item-details">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
                             </div>
@@ -91,48 +112,150 @@
 
                         <div class="section-item">
                             <h3>Kenyér</h3>
-                            <div class="section-item-picture-wrapper">
-                                <img src="/../assets/images/bread-1.jpg" alt="">
-                                <img src="/../assets/images/bread-2.jpg" alt="">
-                            </div>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
                             <div class="section-item-details">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
                             </div>
                         </div>
 
+                    </div>
+                    <div class="content-footer">
+                        <button>
+                            <p>Többi megjelelenítése</p>
+                            <img src="/../assets/icons/visibility-red.svg" alt="">
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section class="medium">
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <h1>Üzleteink</h1>
+                    </div>
+                    <div class="content grid">
+                        <div class="section-item">
+                            <h3>Magyon jó fajta és finomn nagyon jó</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details store">
+                                <p>Cím: <span>1142 Budapest, Korong utca 211-213</span></p>
+                                <p>Tel.: <span>1142 Budapest, Korong utca 211-213</span></p>
+                                <p>Nyitvatartás:</p>
+                                <div>
+                                    <p>Nap</p>
+                                    <p>Idő</p>
+                                    <p>H.</p>
+                                    <p>14:00 - 15:00</p>
+                                </div>
+                                <button>Útvonalterv</button>
+                            </div>
                         </div>
-                    </section>
-
-                    <section>
-                        <h2>Kategória 2</h2>
-                    </section>
-
-                    <section>
-                        <h2>Kategória 3</h2>
-                    </section>
-
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                        <div class="section-item">
+                            <h3>Kenyér</h3>
+                            <img src="/../assets/images/bread-1.jpg" alt="">
+                            <div class="section-item-details">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia dolorum nobis odio error rerum commodi incidunt saepe illo! Obcaecati quasi corrupti quis quos est. Vero a ad itaque deleniti.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </section>
 
-
-                <div id="stores">
-                    <h1>Üzleteink</h1>
-                    <section>
-
-                    </section>
+            <section class="dark">
+                <div class="content-wrapper">
+                    <div class="content-header">
+                        <h1>Elérhetőségeink</h1>
+                    </div>
+                    <div class="content text-extended">
+                        <p>Üzem és Iroda: <span>Nagyon jó hely, sok páncélos kocsi utca 4</span></p>
+                        <p>Telefonszám: <span>+36 70 123 5678</span></p>
+                        <p>E-mail: <span>email.cim@email.com</span></p>
+                    </div>
                 </div>
-
-
-                <div id="contact">
-                    <section>
-                        <h2>Elérhetőség</h2>
-                    </section>
-                </div>
-
-            </div>
+            </section>
 
         </div>
     </main>
-    <footer>zhrtjuj</footer>
+    <footer>
+        <div><img src="/../assets/images/logo.svg" alt="Farkas Vadkovászos Pékség Logo"></div>
+        <div>
+            <p>Farkas Vadkovászos Pékség &copy; 2021</p>
+        </div>
+        <div>
+            <p>Weblapot készítette:</p>
+            <p>Várkonyi Borisz, Wolfram Kristóf</p>
+        </div>
+    </footer>
     <script src="/../js/main-script.js"></script>
 </body>
 </html>
--- js/main-script.js
+++ js/main-script.js
@@ -1,5 +1,10 @@
 const header = document.querySelector("header");
+const mobileNav = document.querySelector("nav#mobile-navigation");
 const showOff = document.getElementById("show-off");
+const openNavButton = document.getElementById("open-nav-button");
+const closeNavButton = document.getElementById("close-nav-button");
+
+var isMobileNavOpen = false;
 
 const showOffOptions = {
     rootMargin: "-145px 0px 0px 0px"
@@ -10,7 +15,9 @@ const showOffObserver = new IntersectionObserver(function (entries, showOffObser
 
         if (!entry.isIntersecting) {
             header.classList.add("scrolled")
-        } else {
+
+        } else if (entry.isIntersecting && !isMobileNavOpen) {
+
             header.classList.remove("scrolled")
         }
 
@@ -18,3 +25,31 @@ const showOffObserver = new IntersectionObserver(function (entries, showOffObser
 }, showOffOptions);
 
 showOffObserver.observe(showOff);
+
+function openMobileNavigation() {
+    isMobileNavOpen = true;
+
+    header.classList.add("scrolled")
+    mobileNav.classList.remove("collapsed")
+
+    openNavButton.classList.toggle("hidden");
+    closeNavButton.classList.toggle("hidden");
+}
+
+function closeMobileNavigation() {
+    isMobileNavOpen = false;
+
+    openNavButton.classList.toggle("hidden");
+    closeNavButton.classList.toggle("hidden");
+
+    mobileNav.classList.add("collapsed")
+
+    if (showOffObserver.isIntersecting) {
+
+        header.classList.add("scrolled")
+
+    } else if (!showOffObserver.isIntersecting) {
+
+        header.classList.remove("scrolled")
+    }
+}
